d03c3d88d00d748d1cedd6136daf81ffec912404
Mohamedmahdy404 <mohamedmahdy3162@gmail.com>
2025-03-27 22:42:45 +0200

    Add architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 
+const routes = [
+  { path: 'home', component: HomeComponent }, 
+
+];
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
