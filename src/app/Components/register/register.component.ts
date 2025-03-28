7465d071105adb58e570f1b98570bf8b51b0206e
RouQandel <rawanqandel62@gmail.com>
2025-03-28 22:34:28 +0200

    Add_Login_RegisterComponent

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
 
+
+export class RegisterComponent {
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  
+  
+  constructor(private router: Router) {} 
+  
+  signUpUser() {
+    console.log(this.firstName && this.lastName && this.email && this.password);
+    if (this.firstName && this.lastName && this.email && this.password) {
+     
+      this.router.navigate(['/Login']);
+    } else {
+      alert('Form is Required');
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
-import {  RouterOutlet } from '@angular/router';
-import { HomeComponent } from './Components/home/home.component';
-import { ProductComponent } from './Components/Product/product.component';
+import {  RouterOutlet ,Router  } from '@angular/router';
+
+
 import { NavbarComponent } from "./shared/NavBar/navbar.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { CommonModule } from '@angular/common';
+
+
+
+export class AppModule {}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent ,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'StylePay';
+  
+  showHeaderFooter: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.showHeaderFooter = !['/Login', '/Register'].includes(this.router.url);
+      console.log('Current URL:', this.router.url);
+    });
+}
 }
