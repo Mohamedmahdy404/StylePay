48c26864a3823ffce1019452c98d05154c46b783
Mohamedmahdy404 <mohamedmahdy3162@gmail.com>
2025-03-28 04:24:25 +0200

    Add routing to Login and Register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductComponent } from './Components/Product/product.component';
 import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
-    { path: 'product', component: ProductComponent }
+    { path: 'product', component: ProductComponent },
+    { path: 'Login', component: LoginComponent },
+    { path: 'Register', component: RegisterComponent }
 ];
